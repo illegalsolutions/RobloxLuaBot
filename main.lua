a4c9070c6360dbd5cd849c7bca81a9a451ef48b9
OnlySploits <47225461+OnlySploits@users.noreply.github.com>
2020-11-18 15:46:27 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -7,11 +7,15 @@ local args = {
 game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
 end
 
-function WalkTo(pos)
-    game.Players.LocalPlayer.Character.Humanoid:MoveTo(Vector3.new(pos))
+function WalkTo(pos1,pos2,pos3)
+    game.Players.LocalPlayer.Character.Humanoid:MoveTo(Vector3.new(pos1,pos2,pos3))
 end
 
 function jump()
     game.Players.LocalPlayer.Character.Humanoid.Jump = true
+end
+
+function sit()
+    game.Players.LocalPlayer.Character.Humanoid.Sit = true
 end
 
